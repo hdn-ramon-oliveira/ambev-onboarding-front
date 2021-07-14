2c72a7e4173ce04405168314d83c2a322cb6aea2
Filipe Poletto <filipepoletto@gmail.com>
2021-07-14 12:24:35 -0300

    Add Button styles on theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -32,6 +32,23 @@ export const theme = extendTheme({
         color: 'primary.500',
       },
     },
+    Button: {
+      baseStyle: {
+        bg: 'secondary.500',
+        color: 'primary.100',
+        fontSize: '14px',
+        fontWeight: 'bold',
+        lineHeight: '20px',
+        padding: '16px 32px',
+      },
+      variants: {
+        outline: {
+          bg: 'primary.100',
+          color: 'secondary.500',
+          borderColor: 'secondary.500',
+        },
+      },
+    },
   },
   styles: {
     global: {
